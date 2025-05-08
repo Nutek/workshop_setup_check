1b3ec33f295bfae2ede1bec8593eddba6a1a4208
Maciej Broda <nutek12@gmail.com>
2025-05-09 00:27:29 +0200

    No need for dummy test to suppres error

--- workshop_utils/test_workshop_utils_main.cpp
+++ workshop_utils/test_workshop_utils_main.cpp
@@ -1,7 +1,3 @@
 #define BOOST_TEST_MODULE test workshop utils
 
 #include <boost/test/unit_test.hpp>
-
-#include "boost/test/unit_test_suite.hpp"
-
-BOOST_AUTO_TEST_CASE(works_well) { BOOST_TEST(false); }
