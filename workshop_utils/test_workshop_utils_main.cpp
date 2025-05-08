591af0303cfa4dcca3c2c39bac6d92fc8c59e56f
Maciej Broda <nutek12@gmail.com>
2025-05-08 22:44:45 +0200

    First attempt to define boost test

---
+++ workshop_utils/test_workshop_utils_main.cpp
@@ -0,0 +1,7 @@
+#define BOOST_TEST_MODULE test workshop utils
+
+#include <boost/test/unit_test.hpp>
+
+#include "boost/test/unit_test_suite.hpp"
+
+BOOST_AUTO_TEST_CASE(works_well) { BOOST_TEST(false); }
